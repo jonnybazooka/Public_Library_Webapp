f1f5cf30bfd1227517f3e50ca7dce6763a7d440c
Tomasz Gieraltowski <tgieraltowski@gmail.com>
2019-07-02 22:51:56 +0200

    postgres connections restructured

--- src/main/java/org/tgieralt/servlets/LibraryDBServlet.java
+++ src/main/java/org/tgieralt/servlets/LibraryDBServlet.java
@@ -1,13 +1,15 @@
 package org.tgieralt.servlets;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.tgieralt.models.Book;
-import org.tgieralt.postgres.ConnectionSQL;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -18,12 +20,29 @@ import java.util.List;
 
 public class LibraryDBServlet extends HttpServlet {
 
+    private DataSource dataSource;
+
+    @Override
+    public void init() throws ServletException {
+        String dbUrl = System.getenv("JDBC_DATABASE_URL");
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        dataSource = new HikariDataSource(config);
+    }
+
+    private Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection = null;
         List<Book> books = new ArrayList<>();
-        try (Connection connection = ConnectionSQL.getDatasource().getConnection()){
+        try {
+            connection = getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM books");
+            connection.close();
             while (resultSet.next()) {
                 String title = resultSet.getString("title");
                 String author = resultSet.getString("author");
@@ -35,22 +54,41 @@ public class LibraryDBServlet extends HttpServlet {
             dispatcher.forward(req, resp);
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try (Connection connection = ConnectionSQL.getDatasource().getConnection()){
+        Connection connection = null;
+        try {
+            connection = getConnection();
             String title = req.getParameter("title");
             String author = req.getParameter("author");
             String isbn = req.getParameter("isbn");
             Statement statement = connection.createStatement();
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS books (title VARCHAR(30), author VARCHAR(30), isbn VARCHAR(15))");
             statement.executeUpdate("INSERT INTO books VALUES ('" + title + "', '" + author + "', " + isbn + ")");
+            connection.close();
             RequestDispatcher dispatcher = req.getRequestDispatcher("library.jsp");
             dispatcher.forward(req, resp);
         } catch (SQLException e) {
             e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
