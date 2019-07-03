e3e80cefa2aee804f63ddef33fc59560f96e5cf2
Tomasz Gieraltowski <tgieraltowski@gmail.com>
2019-07-03 10:55:42 +0200

    connection pool set to 1

--- src/main/java/org/tgieralt/servlets/LibraryDBServlet.java
+++ src/main/java/org/tgieralt/servlets/LibraryDBServlet.java
@@ -22,7 +22,7 @@ public class LibraryDBServlet extends HttpServlet {
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(dbUrl);
-        config.setMaximumPoolSize(10);
+        config.setMaximumPoolSize(1);
         DataSource dataSource = new HikariDataSource(config);
         return dataSource.getConnection();
     }
